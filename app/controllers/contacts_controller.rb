7711ab0639bf045195dab8b878c972f1e779e551
Sam Huang <sshuang97@gmail.com>
2021-03-31 18:33:17 +0000

    trigger the maielr from the controller, not sure if previous commit worked

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -23,5 +23,5 @@ class ContactsController < ApplicationController
     private
         def contact_params 
             params.require(:contact).permit(:name, :email, :comments)
-        end 
+        end  
 end 
